f033c2361186d52be59b96fad999600e4ce4992b
Jason Mabry <jmabry@mymabry.com>
2016-05-26 10:27:28 -0400

    bump phoenix version

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Addict.Mixfile do
 
   defp deps do
     [{:cowboy, "~> 1.0"},
-     {:phoenix, "~> 1.1"},
+     {:phoenix, "~> 1.2"},
      {:ecto, "~> 1.1"},
      {:comeonin, "~> 2.1" },
      {:mailgun, "~> 0.1"},
